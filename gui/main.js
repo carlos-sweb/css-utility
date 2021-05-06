bc2ab02f1c118b79f0c34cfd8ddd79a322efa58c
carlos-sweb <c4rl0sill3sc4@gmail.com>
2021-05-06 11:35:29 -0400

    update

--- gui/main.js
+++ gui/main.js
@@ -1,16 +1,19 @@
+const { WSAETIMEDOUT } = require('constants');
 const { app, BrowserWindow } = require('electron')
 const path = require('path')
 
 function createWindow () {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    show:false,    
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
   })
+  win.loadFile('../index.html')
+  win.setMenu(null)
+  win.maximize()
+  win.show()
 
-  win.loadFile('index.html')
 }
 
 app.whenReady().then(() => {
